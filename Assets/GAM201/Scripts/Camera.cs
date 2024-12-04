4e1d69498c53f890ab38883fe514c30029e5faa3
nminh123 <caonhatminh245@gmail.com>
2024-12-05 00:29:09 +0700

    change character, add package,...

--- Assets/GAM201/Scripts/Camera.cs
+++ Assets/GAM201/Scripts/Camera.cs
@@ -2,22 +2,13 @@
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;        // Mục tiêu để theo dõi (nhân vật)
-    public Vector3 offset;          // Khoảng cách giữa camera và mục tiêu
-    public float smoothSpeed = 0.125f;  // Tốc độ di chuyển camera mượt
+    public Transform target;
 
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
     void LateUpdate()
     {
-        if (target == null) return;
-
-        // Xác định vị trí mới cho camera
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-
-        // Cập nhật vị trí camera
-        transform.position = smoothedPosition;
-
-        // Giữ hướng nhìn của camera
+        transform.position = target.position + offset;
         transform.LookAt(target);
     }
 }
